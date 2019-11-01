f3e5e16afea37987f44563090af3f05294bd2579
Rob <robd@vt.edu>
2019-11-01 15:09:24 -0400

    styles and functions

--- src/inject/contentScript.js
+++ src/inject/contentScript.js
@@ -8,37 +8,57 @@ chrome.extension.sendMessage({}, function(response) {
 			button.setAttribute("id", "rssbutton");
 		    button.appendChild(text);
 		    main.appendChild(button);
-			//button.onclick = rss;
-
-			var link = document.createElement("p");
-			for (var arrScripts = document.getElementsByTagName('script'), i = 0; i < arrScripts.length; i++) {
-			    if (arrScripts[i].textContent.indexOf('browseId') != -1) {
-			        var channelId = arrScripts[i].textContent.match(/\"browseId\"\s*\:\s*\"(.*?)\"/)[1];
-			        link.innerHTML = channelRss = 'https://www.youtube.com/feeds/videos.xml?channel_id=' + channelId;
-					break;
-			    }
-			}
+			var rssDiv = document.createElement("div");
+			rssDiv.setAttribute("id", "rssDiv");
+			var link = document.createElement("input");
+			link.setAttribute("id", "linkInputBox")
+			// for (var arrScripts = document.getElementsByTagName('script'), i = 0; i < arrScripts.length; i++) {
+			//     if (arrScripts[i].textContent.indexOf('browseId') != -1) {
+			//         var channelId = arrScripts[i].textContent.match(/\"browseId\"\s*\:\s*\"(.*?)\"/)[1];
+			//         link.value = 'https://www.youtube.com/feeds/videos.xml?channel_id=' + channelId;
+			// 		console.log(link.value);
+			//     }
+			// }
+			link.value = document.links[9].href.split("/")[4];
 			var info = document.getElementById('info');
-			info.appendChild(link);
-			link.style.display = "none";
-
+			rssDiv.appendChild(link);
+			info.appendChild(rssDiv);
+			//rssDiv.style.display = "none";
+			$("#rssDiv").hide();
 			button.addEventListener('click', function() {
-            	rss(link);
+            	rss(rssDiv);
         	});
+
+			document.addEventListener('DOMNodeInserted', function (event) {
+				//getLink();
+			});
 		}
 	}, 10);
 });
 
 
 function rss(rssinfo) {
+	var slideSpeed = 200;
+
 	if (rssinfo.style.display === "none") {
-    	rssinfo.style.display = "block";
+    	$("#rssDiv").show(slideSpeed);
+		var copyText = document.getElementById("linkInputBox");
+	  	copyText.select();
+	  	copyText.setSelectionRange(0, 99999);
+	  	document.execCommand("copy");
+		copyText.blur();
+		$("#rssDiv").delay(1000).hide(slideSpeed);
   	} else {
-    	rssinfo.style.display = "none";
+    	$("#rssDiv").hide(slideSpeed);
   	}
-	styles();
 }
 
-function styles() {
-	//this.setAttribute("style", "background-color:#fff");
+function getLink() {
+	for (var arrScripts = document.getElementsByTagName('script'), i = 0; i < arrScripts.length; i++) {
+		if (arrScripts[i].textContent.indexOf('browseId') != -1) {
+			var channelId = arrScripts[i].textContent.match(/\"browseId\"\s*\:\s*\"(.*?)\"/)[1];
+			console.log('https://www.youtube.com/feeds/videos.xml?channel_id=' + channelId);
+			break;
+		}
+	}
 }
